449564aedf011e9c7a2b00abef1128ba6baf306e
Mulyana <mulcoding@gmail.com>
2024-01-04 10:51:29 +0700

    portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    screens: {
+      a: "480px",
+      b: "680px",
+      c: "860px",
+      c: "1024px",
+    },
+  },
+  plugins: [],
+};
